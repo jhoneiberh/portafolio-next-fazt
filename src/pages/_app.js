c5aff5550a1f75ca69c67f2461908993daa10d18
Jhon Eiber Hurtado <jhoneiberh@gmail.com>
2023-07-31 12:17:36 -0500

    add blog

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css';
+import React from 'react';
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
+import '../../global.css';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="description" content="Ryan Ray simple Portfolio website" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"  />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
